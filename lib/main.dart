d62f5a3d4c5c6edc666bb7068f324728fdf8a112
Allan Davis <cajun.code@gmail.com>
2019-05-20 22:56:41 -0400

    Completed Chalange 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              Container(
+                width: 100,
+                color: Colors.blue,
+              ),
+            ],
+        ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
